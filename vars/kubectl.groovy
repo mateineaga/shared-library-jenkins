5ba60b6fee3f9d7b717aec1680154b272dc7cae3
matei neaga <mateineaga@outlook.com>
2025-08-01 18:06:16 +0300

    Added JSON response

--- vars/kubectl.groovy
+++ vars/kubectl.groovy
@@ -11,6 +11,51 @@ String getReleaseVersion(Map stageParams = [:]) {
     ).trim()
 }
 
+String getPatchJsonResponse(Map stageParams = [:]) {
+    String namespace    = stageParams.namespace    ?: 'default'
+    String resourceName = stageParams.resourceName
+    String resourceType = stageParams.resourceType ?: 'dr'
+    String releaseVersion = stageParams.releaseVersion 
+
+    return sh( 
+        script: '''#!/bin/bash
+        kubectl get ${resourceType} -n ${namespace} ${resourceName}-${releaseVersion} -o=json | 
+            jq '{
+                "spec": {
+                    "template": {
+                        "spec": {
+                            "containers": [{
+                                "image": .spec.template.spec.containers[0].image,
+                                "name": .spec.template.spec.containers[0].name,
+                                "resources": {   
+                                    "limits": {
+                                        "cpu": .spec.template.spec.containers[0].resources.limits.cpu,
+                                        "ephemeral-storage": .spec.template.spec.containers[0].resources.limits["ephemeral-storage"],
+                                        "memory": .spec.template.spec.containers[0].resources.limits.memory
+                                    },
+                                    "requests": {
+                                        "cpu": .spec.template.spec.containers[0].resources.requests.cpu,
+                                        "ephemeral-storage": .spec.template.spec.containers[0].resources.requests["ephemeral-storage"],
+                                        "memory": .spec.template.spec.containers[0].resources.requests.memory
+                                    }
+                                }
+                            }]
+                        }
+                    }
+                }
+            }'
+        ''',
+        returnStdout: true
+    ).trim()
+}
+
+// env.JSON_RESPONSE = kubectl.getPatchJsonResponse(
+//     namespace: "${SOURCE_NAMESPACE}",
+//     resourceName: "${SERVICE_NAME}",
+//     resourceType: 'deployment'
+//     releaseVersion: "${env.RELEASE_VERSION}"
+// )
+
 void patchUpdateFileJSON(Map stageParams = [:]) {
     String namespace    = stageParams.namespace    ?: 'default'
     String options      = stageParams.options      ?: ''
